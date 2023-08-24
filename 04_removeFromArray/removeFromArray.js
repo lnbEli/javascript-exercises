c6bdc7c3fa671c06df6e8a5ae74bdc00aeee36e5
lnbEli <137424458+lnbEli@users.noreply.github.com>
2023-08-24 13:32:35 +0200

    finished

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (arr, ...value) {
+  for (val of value) {
+    if (arr.indexOf(val) !== -1) {
+      arr.splice(arr.indexOf(val), 1);
+    }
+  }
+  return arr;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
-
+const sumAll = function (num1, num2) {
+  let total = 0;
+  if (
+    num1 < 0 ||
+    num2 < 0 ||
+    typeof num1 !== "number" ||
+    typeof num2 !== "number"
+  ) {
+    return "ERROR";
+  }
+  for (
+    let i = num1 <= num2 ? num1 : num2;
+    i <= (num1 <= num2 ? num2 : num1);
+    i++
+  ) {
+    total += i;
+  }
+  return total;
 };
 
 // Do not edit below this line
